67735f09b8f8c20aa0e6fee7bc44190c7a46172d
배준혁 <philbae0@gmai.com>
2024-12-10 00:26:27 +0900

    website

---
+++ javascript.js
@@ -0,0 +1,26 @@
+
+let currentSlide = 0;
+
+const slides = document.querySelector('.slides');
+const totalSlides = document.querySelectorAll('.slide').length;
+
+function showSlide(index) {
+    if (index >= totalSlides) {
+        currentSlide = 0;
+    } else if (index < 0) {
+        currentSlide = totalSlides - 1;
+    } else {
+        currentSlide = index;
+    }
+    slides.style.transform = `translateX(-${currentSlide * 100}%)`;
+}
+
+function nextSlide() {
+    showSlide(currentSlide + 1);
+}
+
+function prevSlide() {
+    showSlide(currentSlide - 1);
+}
+
+setInterval(nextSlide, 5000);
